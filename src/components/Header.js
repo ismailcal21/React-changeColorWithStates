8a3ed80a1de0a928e2bc8f71a1388278fc30dc0f
ismailcal21 <ismailcal21@gmail.com>
2022-10-10 12:27:04 -1200

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = (props) => {
+  const [changeColor, setChangeColor] = useState(false);
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-danger">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+      <nav
+        className={`navbar navbar-expand-lg bg-${
+          changeColor === true ? "success" : "danger"
+        }`}
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
             Navbar
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -17,24 +22,32 @@ const Header = (props) => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
                   {props.input}
                 </a>
               </li>
-              <li class="nav-item dropdown"></li>
+              <li className="nav-item dropdown"></li>
             </ul>
           </div>
         </div>
+        <button
+          type="button"
+          className="btn btn-primary mx-5"
+          id="basic-addon2"
+          onClick={() => setChangeColor(!changeColor)}
+        >
+          ChangeColor
+        </button>
       </nav>
     </div>
   );
